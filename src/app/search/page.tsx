05bb0b8165a8a708a746ad8542b39e1dd269215e
leesanghyup <leesanghyep@naver.com>
2025-03-03 18:59:53 +0900

    페이지 라우팅 세팅

---
+++ src/app/search/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q: string }>;
+}) {
+  const { q } = await searchParams;
+  console.log(q);
+  return <div>Search 페이지 : {q}</div>;
+}
